669c1a2647f60248cc1bee24d9479a6084deee42
phellipsdevops <phellips@hotmail.com>
2023-07-07 23:49:11 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ Console.log("Rodando o sistema de gerenciamento de jogos")
 Console.log("Rodando o sistema de gerenciamento de jogos")
 //mais um console log de teste:
 Console.log("Alguem fez esse commit sem você ver na aula")
+console.log("Enviando para o Github")
