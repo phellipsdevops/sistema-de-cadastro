a795beeae0b2b29e222781a3b96268d5433c272b
phellipsdevops <phellips@hotmail.com>
2023-07-08 00:27:18 -0300

    voltando para status anterior

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ Console.log("Rodando o sistema de gerenciamento de jogos")
 Console.log("Rodando o sistema de gerenciamento de jogos")
 //mais um console log de teste:
 Console.log("Alguem fez esse commit sem você ver na aula")
-console.log("Enviando para o Github")
