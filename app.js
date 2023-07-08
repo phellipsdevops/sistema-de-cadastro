64c8d95c3aff34d68fda5c0df8ac51a8edb9f967
phellipsdevops <107215222+phellipsdevops@users.noreply.github.com>
2023-07-07 23:40:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 Console.log("Rodando o sistema de gerenciamento de jogos")
 Console.log("Rodando o sistema de gerenciamento de jogos")
+//mais um console log de teste:
+Console.log("Alguem fez esse commit sem você ver na aula")
