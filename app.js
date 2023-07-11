de8cbc623cddb10c09d54b3de32b51c5e7b6afbd
phellipsdevops <phellips@hotmail.com>
2023-07-11 17:26:33 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 Console.log("Rodando o sistema de gerenciamento de jogos")
 Console.log("Rodando o sistema de gerenciamento de jogos")
 //mais um console log de teste:
-Console.log("Alguem fez esse commit sem você ver na aula")
